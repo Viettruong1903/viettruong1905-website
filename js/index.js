6fbe7def700f9a441be20d4f47c08524f62ea68c
Viettruong1903 <114872944+Viettruong1903@users.noreply.github.com>
2025-02-06 16:57:06 -0700

    adding header, buttons, mediaqueries file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 console.log('Hello World');
 
+let btnHamburger = document.querySelector('.hamburger');
+
+btnHamburger.addEventListener('click', function() {
+  this.classList.toggle('active');
+});
+
 function scrollToTop() {
   window.scrollTo({
     top: 0,
