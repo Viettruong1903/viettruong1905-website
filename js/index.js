4005578f4760815e5bb226057bbf4ee791cc6fd9
Viettruong1903 <114872944+Viettruong1903@users.noreply.github.com>
2025-02-07 13:49:06 -0700

    finish header

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
 console.log('Hello World');
 
+let body = document.body;
+let overlay = document.querySelector('.overlay');
+
 let btnHamburger = document.querySelector('.hamburger');
+let closeHamburger = document.querySelector('.close-hamburger-icon');
+let dropdownMenu = document.querySelector('.dropdown-menu');
 
 btnHamburger.addEventListener('click', function() {
-  this.classList.toggle('active');
+  dropdownMenu.classList.add('active');
+  overlay.classList.add('active');
+  body.style.overflow = 'hidden';
+});
+
+closeHamburger.addEventListener('click', function() {
+  dropdownMenu.classList.remove('active');
+  overlay.classList.remove('active');
+  body.style.overflow = '';
+})
+
+overlay.addEventListener('click', function () {
+  dropdownMenu.classList.remove('active');
+  overlay.classList.remove('active');
+  body.style.overflow = '';
 });
 
 function scrollToTop() {
